080a9d0f010c70fbc8e1cb3e11d29f42db8b197e
Marat Baikin <3371335@gmail.com>
2022-12-05 06:49:12 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500;
 const URL = 'https://restcountries.com/v3.1/name/';
 
 const refs = {
@@ -11,42 +12,85 @@ const refs = {
   countryInfo: document.querySelector('.country-info'),
 };
 
+let contriesList = [];
+
+const markupFullInfo = ({ flags, name, capital, population, languages }) =>
+  `<img src="${flags.svg}" alt="flag" width="30px" class="country-info-img">
+              <span class="country-info-name">${name.official}</span>  
+              <ul class="country-info-list">
+                <li class="country-info-item"><strong>Capital: </strong>${capital}</li>
+                <li class="country-info-item"><strong>Population: </strong>${population}</li>
+                <li class="country-info-item"><strong>Languages: </strong>${Object.values(
+                  languages
+                )}</li>
+              </ul>`;
+const markupListShortInfo = ({
+  flags,
+  name,
+  capital,
+  population,
+  languages,
+}) => `<li class="country-item">
+    <img src="${flags.svg}" alt="flag" width="30px" class="country-item-img">
+    <span class="country-item-name">${name.official}</span>
+  </li>`;
+
+const clearMarkup = () => {
+  refs.countryInfo.innerHTML = '';
+  refs.countryInput.innerHTML = '';
+};
+
+const renders = () => {
+  const renderFullInfo = contriesList.map(markupFullInfo);
+  const renderListShortInfo = contriesList.map(markupListShortInfo);
+  clearMarkup();
+  if (contriesList.length === 1) {
+    refs.countryInfo.insertAdjacentHTML('beforeend', renderFullInfo.join(''));
+  } else {
+    refs.countryInfo.insertAdjacentHTML(
+      'beforeend',
+      renderListShortInfo.join('')
+    );
+  }
+};
+
 const handleInput = e => {
   const CountryName = e.target.value;
 
   fetchCountries(CountryName.trim());
 };
 
-
 function fetchCountries(name) {
   fetch(`${URL}/${name}?fields=name,flags,capital,population,languages`)
-    
     .then(response => {
       return response.json();
     })
-    .then(country => {
-      console.log(country);
+    .then(countries => {
+      console.log(countries);
 
-      const { flags, name, capital, population, languages } = country[0];
-      const Countrylang = Object.values(languages);
-      const markupFullInfo = `<img src="${flags.svg}" alt="flag" width="30px" class="country-info-img">
-            <span class="country-info-name">${name.official}</span>  
-            <ul class="country-info-list">
-              <li class="country-info-item"><strong>Capital: </strong>${capital}</li>
-              <li class="country-info-item"><strong>Population: </strong>${population}</li>
-              <li class="country-info-item"><strong>Languages: </strong>${Countrylang}</li>
-            </ul>`;
-      console.log(markupFullInfo);
-      refs.countryInfo.insertAdjacentHTML('beforeend', markupFullInfo);
+      if (countries.length > 10) {
+        Notiflix.Notify.info(
+          `Too many matches found. Please enter a more specific name.`,
+          {
+            clickToClose: true,
+            timeout: 3000,
+          }
+        );
+      }
+
+      contriesList = countries;
+      renders();
     })
     .catch(error => {
       console.log(error);
+      Notiflix.Notify.failure(`Oops, there is no country with that name`, {
+        clickToClose: true,
+        timeout: 3000,
+      });
     });
 }
 
-// `<li class="country-item">
-//         <img src="${flags.svg}" alt="flag" class="country-img">
-//         <span class="country-info-name">${name.official}</span>
-//       </li>`
-
-refs.countryInput.addEventListener('input', handleInput);
+refs.countryInput.addEventListener(
+  'input',
+  debounce(handleInput, DEBOUNCE_DELAY)
+);
